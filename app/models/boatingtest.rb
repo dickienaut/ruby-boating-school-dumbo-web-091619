b59f3f8bfaae2d22be8cd0061def4beae80280e7
Lyle Dickie <lyle.e.dickie@gmail.com>
2019-09-23 11:30:15 -0400

    Finished all classes and methods

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,20 @@
 class BoatingTest
 
+  attr_reader :student, :instructor, :test_name
+  attr_accessor :test_status
+  @@all = []
+
+  def initialize(student, instructor, test_name, test_status)
+  # * should initialize with Student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
+    @student = student
+    @instructor = instructor
+    @test_name = test_name
+    @test_status = test_status
+    @@all << self
+  end
+
+  def self.all
+  # * `BoatingTest.all` returns an array of all boating tests
+    @@all
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,43 @@
 class Student
 
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    # * should initialize with `first_name`
+    @name = name
+    @@all << self
+  end
+
+
+  def self.all
+    # * `Student.all` should return all of the student instances
+    @@all
+  end
+
+
+  def add_boating_test(test_name, test_status, instructor)
+  # * `Student#add_boating_test` should initialize a new boating test with a Student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
+
+    # student, instructor, test_name, test_status
+    BoatingTest.new(self, instructor, test_name, test_status)
+  end
+
+
+  def self.find_student(name)
+  # * `Student.find_student` will take in a first name and output the student (Object) with that name
+    Student.all.filter { |student| student.name == name }
+  end
+
+
+  def all_student_tests
+    # method to find all instances of tests that a student has taken
+    BoatingTest.all.filter { |test| test.student == self }
+  end
+
+
+  def grade_percentage
+  # * `Student#grade_percentage` should return the percentage of tests that the student has passed, a Float (so if a student has passed 3 / 9 tests that they've taken, this method should return the Float `33.33`)
+    all_student_tests.filter { |test| test.test_status == 'passed'}.size.to_f / all_student_tests.size
+  end
 end
